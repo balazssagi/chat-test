42a1f3ec2a9549e7661db669d1e62165e725d87e
Balázs Sági <sagi.balazs@logiscool.com>
2021-12-04 20:35:50 +0100

    sadA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SockJsClient from 'react-stomp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
+      messages: []
+    }
+  }
+
+
+  sendMessage = (msg) => {
+    this.clientRef.sendMessage('/topics/all', msg);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+        <input value={this.state.value} onChange={e => {this.setState({value: e.target.value})}}/>
+        <button onClick={() => {this.sendMessage(this.state.value)}}>send</button>
+        </div>
+        <div>
+          <pre>{JSON.stringify(this.state.messages)}</pre>
+        </div>
+        <SockJsClient url='https://react-websocket.herokuapp.com/handler' topics={['/topics/all']}
+            onMessage={(msg) => { this.setState({messages: [...this.state.messages, msg]}); console.log(msg); }}
+            ref={ (client) => { this.clientRef = client }} />
+      </div>
+    );
+  }
 }
-
-export default App;
